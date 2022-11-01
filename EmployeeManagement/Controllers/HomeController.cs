89fc73555ea2235b830b88465ab20644ad862ab8
Clementina <folukeabogoh@gmail.com>
2022-11-01 16:11:50 +0100

    working on edit

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -53,19 +53,19 @@ namespace EmployeeManagement.Controllers
             return View(employeeEditViewModel);
         }
 
-        [HttpPost]
-        public IActionResult Create(EmployeeCreateViewModel model)
+         [HttpPost]
+        public IActionResult Edit(EmployeeEditViewModel model)
         {
             if (ModelState.IsValid)
             {
-                string uniqueFileName = null;
-                if (model.Photo != null)
-                {
-                    string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
-                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
-                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
-                }
+                Employee employee = _employeeRepository.GetEmployee(model.Id);
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+                string uniqueFileName = ProcessUploadedFile(model);
+
+
+
                 Employee newEmployee = new Employee
                 {
                     Name = model.Name,
@@ -79,6 +79,42 @@ namespace EmployeeManagement.Controllers
             }
 
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeeCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniqueFileName = ProcessUploadedFile(model);
+
+                Employee newEmployee = new Employee
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    PhotoPath = uniqueFileName
+                };
+                _employeeRepository.Add(newEmployee);
+
+                return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+
+            return View();
+        }
+
+
+        private string ProcessUploadedFile(EmployeeEditViewModel model){
+            string uniqueFileName = null;
+                if (model.Photo != null)
+                {
+                    string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+
+            return uniqueFileName;
         }
 
     }
