e2718cc153eb37dac5292c8b1dac4c88d2583b55
Clementina <folukeabogoh@gmail.com>
2022-11-03 16:52:20 +0100

    added exception handling and 404 page

--- EmployeeManagement/Controllers/ErrorController.cs
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace EmployeeManagement.Controllers
 {
@@ -14,10 +15,14 @@ namespace EmployeeManagement.Controllers
 
         public IActionResult Index(int statusCode)
         {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
             switch (statusCode)
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry the resource you requested could not be found";
+                    ViewBag.Path = statusCodeResult.OriginalPath;
+                    ViewBag.QS = statusCodeResult.OriginalQueryString;
                     break;
             }
             return View("NotFound");
