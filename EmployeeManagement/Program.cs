689d0fd830c6763b56546e445856de9dc4e95695
Clementina <folukeabogoh@gmail.com>
2022-11-14 16:41:24 +0100

    added identityDbcontext

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,5 +1,6 @@
 using EmployeeManagement.Models;
 using EmployeeManagement.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("EmployeeDBConnection");
 
 builder.Services.AddDbContextPool<AppDbContext>(db => db.UseSqlServer(connectionString));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
 builder.Services.AddMvc(options => options.EnableEndpointRouting = false).AddXmlSerializerFormatters();
 builder.Services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
 
